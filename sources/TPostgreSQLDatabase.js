3781024913df131eb38270d999d1980c0942de2e
Tristan VALCKE <valcketristan@gmail.com>
2020-11-03 15:40:01 +0100

    fix(tpostgresqldatabase): fix postgresql driver initialisation and close

--- sources/TPostgreSQLDatabase.js
+++ sources/TPostgreSQLDatabase.js
@@ -8,14 +8,31 @@
  *
  */
 
-import PostgreSQL            from 'pg-promise'
 import { TAbstractDatabase } from 'itee-database'
+import { isDefined }         from 'itee-validators'
+import PostgreSQL            from 'pg-promise'
 
-const PostgreSQLDriver = PostgreSQL( {} )
+//const PostgreSQLDriver = PostgreSQL( {} )
 
 class TPostgreSQLDatabase extends TAbstractDatabase {
 
+    static formatConnectionString ( parameters ) {
+
+        let connectionString                           = ''
+        const { user, password, host, port, database } = parameters
+
+        if ( isDefined( user ) && isDefined( password ) ) {
+            connectionString = `postgres://${ user }:${ password }@${ host }:${ port }/${ database }`
+        } else {
+            connectionString = `postgres://${ host }:${ port }/${ database }`
+        }
+
+        return connectionString
+
+    }
+
     constructor ( parameters = {} ) {
+
 
         const _parameters = {
             ...{
@@ -25,29 +42,18 @@ class TPostgreSQLDatabase extends TAbstractDatabase {
             },
             ...parameters,
             ...{
-                driver: PostgreSQLDriver
+                driver: PostgreSQL( {} )( TPostgreSQLDatabase.formatConnectionString( parameters ) )
             }
         }
 
         super( _parameters )
 
+        this._user     = _parameters.user
+        this._password = _parameters.password
         this._host     = _parameters.host
         this._port     = _parameters.port
         this._database = _parameters.database
 
-    }
-
-    close ( /*onCloseCallback*/ ) {}
-
-    connect () {
-
-        this._driver.one( ` SELECT 1 `, [] )
-            .then( ( data ) => {
-                console.log( `PostgreSQL at ${this._host}:${this._port}/${this._database} is connected ! ${data}` )
-            } )
-            .catch( ( error ) => {
-                console.log( 'PostgreSQL - Connection error ', error )
-            } )
     }
 
     init () {
@@ -55,7 +61,26 @@ class TPostgreSQLDatabase extends TAbstractDatabase {
 
     }
 
+    connect () {
+
+        this.driver.one( ` SELECT 1 `, [] )
+            .then( () => {
+                console.log( `PostgreSQL at postgres://${ this._host }:${ this._port }/${ this._database } is connected !` )
+            } )
+            .catch( ( error ) => {
+                console.log( 'PostgreSQL - Connection error ', error )
+            } )
+
+    }
+
     on ( /*eventName, callback*/ ) {}
+
+    close ( onCloseCallback ) {
+
+        this.driver.end()
+        onCloseCallback()
+
+    }
 
 }
 
